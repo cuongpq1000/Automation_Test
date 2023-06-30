a3e6716c19847cd0d811f642b4c054db50691f44
NguyenSongHoangHuy <songhoanghuy@gmail.com>
2023-06-30 09:05:43 +0700

    excel

--- excel.py
+++ excel.py
@@ -1,9 +1,21 @@
-import pandas
+import pandas as pd
 
+def run():
+    excel = readExcel('Demo.xlsx', 'Sheet1')
+    print("get cell [0,1]", getCell(excel, 0, 1))
+    print("get row [2]", getRow(excel, 2))
+    print("get col [1]", getCol(excel, 1))
 
-def readExcel():
-    excel_data_df = pandas.read_excel('Demo.xlsx', sheet_name='Sheet1')
-    # print whole sheet data
-    print(excel_data_df)
+def readExcel(name, sheetname):
+    df = pd.read_excel(name, sheet_name=sheetname, dtype=object, header=None)
+    return df
 
-readExcel()
+def getCell(data, row, col):
+    return data.values[row][col]
+
+def getRow(data, index):
+    return data.values[index]
+
+def getCol(data, index):
+    return data[index].tolist()
+run()
