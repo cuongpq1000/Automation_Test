a03e8de292201a893602df05bc05d76333f5bc0a
AnhTuPasona <62977426+NguyenAnhTu4869@users.noreply.github.com>
2023-06-30 11:06:29 +0700

    excel.py

--- excel.py
+++ excel.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import json
+import os
 
 def run():
     excel = readExcel('Demo.xlsx', 'Sheet1')
@@ -19,3 +21,29 @@ def getRow(data, index):
 def getCol(data, index):
     return data[index].tolist()
 run()
+
+def convertExceltoJSON(excelFile, jsonFile):
+    # Get current folder
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # Create folder excel path
+    excelPath = os.path.join(current_dir, "testCase", "excel")
+
+    # Create file excel path
+    excelFilePath = f"{excelPath}\{excelFile}"
+
+    # Read file excel
+    Excel = pd.read_excel(excelFilePath)
+
+    # Convert Excel to JSON
+    Json = Excel.to_json(orient='records')
+
+    # Create folder json path
+    jsonPath = os.path.join(current_dir, "testCase", "json")
+
+    # Create file json path
+    jsonFilePath = f"{jsonPath}\{jsonFile}.json"
+
+    # Create json file
+    out_file = open(jsonFilePath, "w")
+    json.dump(json.loads(Json), out_file, indent = 6)
